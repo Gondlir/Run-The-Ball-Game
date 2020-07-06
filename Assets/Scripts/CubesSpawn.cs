9785b27d4014dabcee7b2edc6902fc37590c1e75
Gondlir <kaoeskt@hotmail.com>
2020-07-06 17:21:53 -0300

    Added cubes grid
    Adicionado cubos em posições aleatorias em forma de grade

---
+++ Assets/Scripts/CubesSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public sealed class CubesSpawn : MonoBehaviour 
+{
+	[SerializeField] private GameObject[] cubesToSpawn;
+	public int linePositionX; // posição da grid no plano X
+	public int linePositionZ; // posição da grid no plano Z
+	public float cubesPositionOffSet = 1f; // distancia dos cubos
+	public Vector3 originPosition = Vector3.zero;
+
+	void Start() 
+	{
+		SpawnCubes();
+	}
+	
+	void SpawnCubes() 
+	{
+		originPosition.z = Random.Range(30, 100);
+        for (int i = 0; i < linePositionX; i++)
+        {
+            for (int j = 0; j < linePositionZ; j++)
+            {
+				Vector3 spawnPositi = new Vector3(i * cubesPositionOffSet, 0, j * cubesPositionOffSet) + originPosition;
+				SpawnObjects(spawnPositi, Quaternion.identity);
+			}
+        }
+	}
+
+	void SpawnObjects(Vector3 positionToSpawn, Quaternion rotationToSpawn) 
+	{
+		int randomIndex = Random.Range(0, cubesToSpawn.Length);
+		GameObject cloneObj = Instantiate(cubesToSpawn[randomIndex], positionToSpawn, rotationToSpawn);
+	}
+}
---
+++ Assets/Scripts/EnemysSpawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class EnemysSpawn : MonoBehaviour {
+
+	[SerializeField] private GameObject[] enemysToSpawn;
+	public int linePositionX; // posição da grid no plano X
+	public int linePositionZ; // posição da grid no plano Z
+	public float cubesPositionOffSet = 1f; // distancia dos cubos
+	public Vector3 originPosition = Vector3.zero;
+	public Vector3 randonPosition;
+
+	void Start () 
+	{
+		SpawnEnemies();
+	}
+	void SpawnEnemies()
+	{
+		originPosition.z = Random.Range(25, 100);
+		linePositionX = Random.Range(1, 6);
+		linePositionZ = Random.Range(1,5);
+
+		for (int i = 0; i < linePositionX; i++)
+		{
+			for (int j = 0; j < linePositionZ; j++)
+			{
+				Vector3 spawnPositi = new Vector3(i * cubesPositionOffSet, 0, j * cubesPositionOffSet) + originPosition;
+				SpawnEnemy(spawnPositi, Quaternion.identity);
+			}
+		}
+	}
+	Vector3 RandonPositions(Vector3 position) 
+	{
+		Vector3 randonPos = new Vector3(originPosition.x, originPosition.y, Random.Range(-originPosition.z, originPosition.z));
+		return randonPos;
+	}
+		
+	void SpawnEnemy(Vector3 positionToSpawn, Quaternion rotationToSpawn)
+	{
+		int randomIndex = Random.Range(0, enemysToSpawn.Length);
+		GameObject cloneObj = Instantiate(enemysToSpawn[randomIndex], positionToSpawn, rotationToSpawn);
+	}
+}
